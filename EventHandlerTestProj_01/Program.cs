c9c6e520b6ab386e4cdc3e4ca6c40f8050b9e14d
romansechin <romansechin@gmail.com>
2024-09-10 09:50:55 +0300

    Добавьте файлы проекта.

---
+++ EventHandlerTestProj_01/Program.cs
@@ -0,0 +1,58 @@
+﻿//Objectives: 
+//•  Make a new project that includes the above code. 
+//•  Add a Ripened event to the CharberryTree class that is raised when the tree ripens. 
+//•  Make a  Notifier  class that knows about the tree (Hint:  perhaps pass it in as a constructor 
+//parameter) and subscribes to its Ripened event. Attach a handler that displays something like “A 
+//charberry fruit has ripened!” to the console window. 
+//•  Make a Harvester class that knows about the tree (Hint: like the notifier, this could be passed as 
+//a constructor parameter) and subscribes to its Ripened event. Attach a handler that sets the tree’s 
+//Ripe property back to false. 
+//•  Update your main method to create a tree, notifier, and harvester, and get them to work together 
+//to grow, notify, and harvest forever.
+
+
+public class CharberryTree
+{
+    private Random _random = new Random();
+    public bool Ripe { get; set; }
+    public event EventHandler<string>? Ripened ;
+    static decimal countOfAttempt = 0M;
+    public bool MaybeGrow()
+    {
+        // Only a tiny chance of ripening each time, but we try a lot! 
+        double nextDouble;
+        ++countOfAttempt;
+
+        if ((nextDouble = _random.NextDouble()) < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            
+            Ripened?.Invoke(this, $" { nextDouble } Ripened! Attempt #:{ countOfAttempt } ");
+            countOfAttempt = 0;
+            return true;
+        }
+        return false;
+    }
+}
+public class Notifier {
+    public Notifier(CharberryTree tree) {
+        tree.Ripened += RipenedHandler;    
+    }
+
+    public void RipenedHandler(object sender, string message) 
+    {        
+        Console.WriteLine($"A { ((CharberryTree)sender).ToString() } has ripened with message: { message }");
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CharberryTree tree = new CharberryTree();
+        Notifier notifier = new Notifier(tree);
+        while (true)
+            if (tree.MaybeGrow() == true)
+                break;
+
+    }
+}
